848a9047eb9e6ca71ec78b20b6b99f88ae0f9964
+ <fadosa.abdirhman@student.moringaschool>
2024-07-05 11:14:16 +0300

    modify

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Code your solution in this file!
+// Code your solution in this file!
 
+// Define companyName as a const with the value 'Scuber'
 const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith';
 
-module.exports = {
-  companyName,
-  mostProfitableNeighborhood,
-  companyCeo,
-};
+// Define mostProfitableNeighborhood using let with the value 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let with the value 'Susan Smith'
+let companyCeo = 'Susan Smith';
